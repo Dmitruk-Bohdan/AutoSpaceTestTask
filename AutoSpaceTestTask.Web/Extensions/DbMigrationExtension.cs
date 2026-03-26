439f4d291080fc7fdb07d807ea31a45ad2e6a9fa
Dmitruk Bohdan <dmitrukb91@gmail.com>
2026-03-26 21:10:47 +0300

    startup migrations refactoring

--- AutoSpaceTestTask.Web/Extensions/DbMigrationExtension.cs
+++ AutoSpaceTestTask.Web/Extensions/DbMigrationExtension.cs
@@ -1,4 +1,5 @@
 ﻿using AutoSpaceTestTask.Database.Context;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace AutoSpaceTestTask.Database.Extensions;
@@ -7,13 +8,30 @@ public static class DbMigrationExtension
 {
     public static WebApplication ApplyDbMigrations(this WebApplication app)
     {
-#if !DEBUG        
-        using (var scope = app.Services.CreateScope())
+        using var scope = app.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        var retryCount = 5;
+        var delay = TimeSpan.FromSeconds(5);
+        for (int i = 0; i < retryCount; i++)
         {
-            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-            db.Database.Migrate();
+            try
+            {
+                db.Database.Migrate();
+                break;
+            }
+            catch (SqlException ex) when (ex.Number == 1801)
+            {
+                if (db.Database.CanConnect())
+                {
+                    continue;
+                }
+                throw;
+            }
+            catch (SqlException) when (i < retryCount - 1)
+            {
+                Thread.Sleep(delay);
+            }
         }
-#endif
         return app;
     }
 }
